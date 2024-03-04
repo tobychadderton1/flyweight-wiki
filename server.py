6eb3849cb0ed8609e36f0a65872ad54bdc98c276
Jacob Campbell <jacobcampbell04@protonmail.com>
2024-03-04 22:35:22 +0000

    Fixed db issue

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from markupsafe import Markup
 from flask_sqlalchemy import SQLAlchemy
-
+from models import Article
 import markdown
 import json
 
@@ -12,8 +12,22 @@ settings = json.load(open("settings.json"))
 
 db = SQLAlchemy(app)
 
+# Models
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # auto assigns names in db
+    name = db.Column(db.Text)
+    content = db.Column(db.Text)
+
+    def __init__(self, name, content):
+        self.name = name
+        self.content = content
+
+
 @app.route("/")
 def home():
+    articles = Article.query
+    print(articles)
     return render_template("home.html", wiki_name=settings["wiki-name"], wiki_logo=settings["wiki-logo"])
 
 @app.route("/article")
@@ -23,7 +37,14 @@ def article(id="test"):
 
 @app.route("/create-article", methods=["GET", "POST"])
 def create_article():
-    return "Create Article Page"
+    if request.method == "POST":
+        print(request.form) # testing code
+        article = Article(name=request.form["name"], content=request.form["content"])
+        db.session.add(article)
+        db.session.commit()
+        return redirect(url_for("home"))
+    else:
+        return render_template("create_article.html")
 
 @app.route("/update-article", methods=["GET", "POST"])
 def update_article():
